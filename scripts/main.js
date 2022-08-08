8c71fbb51fd22f1bb6b3abd8f1051f55d3220df2
Advay Vyas <advay.vyas@gmail.com>
2022-08-08 18:44:20 -0500

    Added new template (correctly) - deleted blank file from incorrect file movement - added template files (pages, assets, etc) - started the cleanup process (more to be done)

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
